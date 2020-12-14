89d253282ece45b55c46fd403cd56fab982ad125
faithakinbobola <faithakinbobola2@gmail.com>
2020-12-14 10:18:32 +0100

    created reservation form, login form and validated them

--- javaScript/login.js
+++ javaScript/login.js
@@ -1,24 +1,8 @@
 let inform = document.querySelector(".information");
-let fname = document.getElementById("fname");
 let email = document.getElementById("email");
 let password = document.getElementById("pswrd");
 
-function nameFieldValidation(event){
-    let nameRegex = /^([a-zA-Z]{2,}) ([a-zA-Z]{2,})$/;
-    if(fname.value === ''){
-        document.getElementById("valid1").innerHTML = "This field is required";
-        event.preventDefault();
-        return false;
-    } else if (!fname.value.match(nameRegex)) {
-        document.getElementById("valid1").innerHTML = "Input not valid";
-        event.preventDefault();
-        return false;
-    } else {
-        document.getElementById("valid1").innerHTML = "";
-        event.preventDefault();
-        return true;
-    }
-}
+
 function emailFieldValidation(event){
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
     if(email.value === ''){
@@ -52,7 +36,6 @@ function passwordFieldValidation(event) {
 }
 
 function information(e) {
-    nameFieldValidation(e);
     emailFieldValidation(e);
     passwordFieldValidation(e);
 }
